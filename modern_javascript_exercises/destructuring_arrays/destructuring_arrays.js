c54b76aaad3876b83b4a90df0b82ec6761d3abdc
smallPterodactyl <119345529+smallPterodactyl@users.noreply.github.com>
2022-12-12 20:27:53 +0000

    Added refactoring exercise via GUI upload

--- modern_javascript_exercises/destructuring_arrays/destructuring_arrays.js
+++ modern_javascript_exercises/destructuring_arrays/destructuring_arrays.js
@@ -1,9 +1,24 @@
 // Instructions can be found in destructuring_arrays.md
 
-export function sequence(order) {
+/*export function sequence(order) {
+
   const first = order[0];
   const second = order[1];
   const third = order[2];
 
   return first + ", " + second + ", " + third;
+} */
+
+/*
+ * Let's assume that 'ordering' means: returning the sequence in the same
+ * order as it was passed in; only ordering 3 units; only ordering strings!
+ * 
+ */ 
+
+export function sequence(order) {
+
+  let [a, b, c, ... rest] = order;
+
+  return (`${a}, ${b}, ${c}`);
+
 }
