65598b1246d5af1621ce0f56ea5e57ffca5f350a
smallPterodactyl <119345529+smallPterodactyl@users.noreply.github.com>
2022-12-13 16:47:24 +0000

    Further simplification
    Cosmetic change: removed spacing to display function on one line

--- modern_javascript_exercises/default_parameters/default_parameters.js
+++ modern_javascript_exercises/default_parameters/default_parameters.js
@@ -1,15 +1,4 @@
 // Instructions can be found in default_parameters.md
 
-/* template literal solution */
-/*export const sayHello = (person) => {
-  return `Hello, ${person === undefined ? "you!" : person + "!"}`;
-};
-*/
 
-/* Now with default parameters*/
-export const sayHello = (person = 'you') => {
-
-  return `Hello, ${person}!`
-
-};
-
+export const sayHello = (person = 'you') => {return `Hello, ${person}!`};
