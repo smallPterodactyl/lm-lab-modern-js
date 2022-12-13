affdc6b59bdba48d5115fe87330dd893a5bb866b
smallPterodactyl <119345529+smallPterodactyl@users.noreply.github.com>
2022-12-13 00:16:13 +0000

    Changed else-if into ternary statement

--- modern_javascript_exercises/ternaries/ternaries.js
+++ modern_javascript_exercises/ternaries/ternaries.js
@@ -1,7 +1,13 @@
 // Instructions can be found in ternaries.md
 
-export function sayHello(name) {
+/*export function sayHello(name) {
   if (name === undefined) {
     return "Hello you!";
   } else return "Hello, " + name + "!";
+} */
+
+export function sayHello (name) {
+
+  return (name === undefined ? "Hello you!" : `Hello, ${name}!`)
+  
 }
