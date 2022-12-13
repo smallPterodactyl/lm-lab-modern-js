62218d2b99485411f13286d6ddb8f398c92f900b
smallPterodactyl <119345529+smallPterodactyl@users.noreply.github.com>
2022-12-13 16:51:50 +0000

    Improved variable naming (destructuring_arrays)

--- modern_javascript_exercises/destructuring_arrays/destructuring_arrays.js
+++ modern_javascript_exercises/destructuring_arrays/destructuring_arrays.js
@@ -1,24 +1,15 @@
 // Instructions can be found in destructuring_arrays.md
 
-/*export function sequence(order) {
-
-  const first = order[0];
-  const second = order[1];
-  const third = order[2];
-
-  return first + ", " + second + ", " + third;
-} */
-
 /*
- * Let's assume that 'ordering' means: returning the sequence in the same
+ * Assume'ordering' means: returning the sequence in the same
  * order as it was passed in; only ordering 3 units; only ordering strings!
  * 
  */ 
 
 export function sequence(order) {
 
-  let [a, b, c, ... rest] = order;
+  const [first, second, third, ... rest] = order;
 
-  return (`${a}, ${b}, ${c}`);
+  return `${third}, ${second}, ${third}`;
 
 }
