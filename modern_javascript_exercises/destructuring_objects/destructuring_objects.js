d49f467e0641343b16da9943fd3ce56944acf205
smallPterodactyl <119345529+smallPterodactyl@users.noreply.github.com>
2022-12-13 16:53:53 +0000

    Removed redundant brackets (destructuring_objects)

--- modern_javascript_exercises/destructuring_objects/destructuring_objects.js
+++ modern_javascript_exercises/destructuring_objects/destructuring_objects.js
@@ -1,18 +1,9 @@
 // Instructions can be found in destructuring_objects.md
 
-/* export function introduction(person) {
-  const name = person.name;
-  const city = person.city;
-  const pet = person.pet;
-
-  return "Hello my name is " + name + " and I live in " + city + " with " + pet;
-}
-*/
-
-export function introduction(person) {
+export function introduction (person) {
     
   const { name, city, pet } = person;
 
-  return (`Hello my name is ${ name } and I live in ${ city } with ${ pet }`);
+  return `Hello my name is ${ name } and I live in ${ city } with ${ pet }`;
 
 }  
